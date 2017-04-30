09cdb41b7883c9d90f949fda45f5297f57ef8767
Martin Corrales <mjcr_023c@hotmail.com>
2017-04-30 19:12:42 -0300

    Se agrega funcionalidad javascript que calcula el año actual y que convierte un número en palabra.

---
+++ js/numberToWord.js
@@ -0,0 +1,74 @@
+/**
+ * numberToWord
+ * @param {type} num
+ * @returns {String}
+ */
+function  numberToWord(num) {
+    switch (num)
+    {
+        case 0:
+            return "cero";
+            break;
+        case 1:
+            return "uno";
+            break;
+        case 2:
+            return "dos";
+            break;
+        case 3:
+            return "tres";
+            break;
+        case 4:
+            return "cuatro";
+            break;
+        case 5:
+            return "cinco";
+            break;
+        case 6:
+            return "seis";
+            break;
+        case 7:
+            return "siete";
+            break;
+        case 8:
+            return "ocho";
+            break;
+        case 9:
+            return "nueve";
+            break;
+        case 10:
+            return "diez";
+            break;
+        case 11:
+            return "once";
+            break;
+        case 12:
+            return "doce";
+            break;
+        case 13:
+            return "trece";
+            break;
+        case 14:
+            return "catorce";
+            break;
+        case 15:
+            return "quince";
+            break;
+        case 16:
+            return "dieciseis";
+            break;
+        case 17:
+            return "diecisiete";
+            break;
+        case 18:
+            return "dieciocho";
+            break;
+        case 19:
+            return "diecinueve";
+            break;
+        case 20:
+            return "veinte";
+        default :
+            return "21";
+    }
+}
